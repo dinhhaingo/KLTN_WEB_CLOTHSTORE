ad0f0fd543fd29167ee6bc4f1444b0f3c312736b
Dinh Hai <>
2020-12-17 17:40:20 +0700

    +payment momo

--- app/controllers/customer.controller.js
+++ app/controllers/customer.controller.js
@@ -285,6 +285,7 @@ exports.findAll = async (req, res) => {
 
 exports.login = async (req, res) => {
     const { username, password } = req.body;
+    console.log(req.body)
     const md5 = require('md5');
     if (!(username && password)) {
         return res.status(400).send({
